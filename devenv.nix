9d1859ccc9650d0d29049a68d37f413bbae340b7
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-03 03:07:29 +0700

    Update via Auto Commit - 2026-07-03 03:07
    Update file: devenv.nix
    
    Changes:
    deleted file mode 100644
    -{
    -  flutter    = import ./flutter/devenv.nix ;
    -  nodejs     = import ./nodejs/devenv.nix  ;
    -  bun        = import ./bun/devenv.nix     ;
    -  rust-basic = import ./rust/devenv.nix     ;
    -}

--- devenv.nix
+++
@@ -1,6 +0,0 @@
-{
-  flutter    = import ./flutter/devenv.nix ;
-  nodejs     = import ./nodejs/devenv.nix  ;
-  bun        = import ./bun/devenv.nix     ;
-  rust-basic = import ./rust/devenv.nix     ;
-}
