1c620c3e0f63ed48ed27f1084845578330cc447a
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-03 03:07:29 +0700

    Update via Auto Commit - 2026-07-03 03:07
    Update file: rust/devenv.nix
    
    Changes:
    -    cargo-watch # auto-rebuild saat file berubah
    -    cargo-edit # cargo add/rm/upgrade
    -    cargo-deny # audit depedency
    -  env.RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
    -    echo "Opsional: Memastikan PKG_CONFIG melihat openssl dari Nix"
    -    echo "export PKG_CONFIG_PATH='${pkgs.openssl.dev}/lib/pkgconfig'"
    -    echo ""

--- rust/devenv.nix
+++ rust/devenv.nix
@@ -12,21 +12,19 @@
   ];
 in {
   packages = [ toolchain ] ++ (with pkgs; [
-    cargo-watch # auto-rebuild saat file berubah
-    cargo-edit # cargo add/rm/upgrade
-    cargo-deny # audit depedency
+    cargo-watch    # auto-rebuild saat file berubah
+    cargo-edit   # cargo add/rm/upgrade
+    cargo-audit   # audit depedency
     pkg-config
     openssl
   ]);
 
   # Tambahkan ini agar rust-analyzer bisa resolve stdlib
-  env.RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+  env.RUST_SRC_PATH   = "${toolchain}/lib/rustlib/src/rust/library";
+  env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
   enterShell = ''
     echo "🦀 Rust Dev Shell (${pkgs.stdenv.hostPlatform.system})"
-    echo ""
-    echo "Opsional: Memastikan PKG_CONFIG melihat openssl dari Nix"
-    echo "export PKG_CONFIG_PATH='${pkgs.openssl.dev}/lib/pkgconfig'"
     echo ""
     rustc --version
     cargo --version
