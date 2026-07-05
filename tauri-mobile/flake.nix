94991c7b388bea897341ea3f3e52790f4e4f0cea
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-05 20:32:56 +0700

    Update via Auto Commit - 2026-07-05 20:32
    Update file: tauri-mobile/flake.nix
    
    Changes:
    -    flake-compat = {
    -      url = "github:edolstra/flake-compat";
    -      flake = false;
    -    };
    -
    -    git-hooks = {
    -      url = "github:cachix/git-hooks.nix";
    -      inputs = {
    -        nixpkgs.follows = "nixpkgs";
    -        flake-compat.follows = "flake-compat";
    -      };
    -    };
    -
    -    devenv = {
    -      url = "github:cachix/devenv";
    -      inputs = {
    -        flake-compat.follows = "flake-compat";
    -        git-hooks.follows = "git-hooks";
    -      };
    -    };
    -
    -    { nixpkgs, flake-utils, ... }@inputs:
    +
    +
    +
    +

--- tauri-mobile/flake.nix
+++ tauri-mobile/flake.nix
@@ -5,31 +5,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     rust.url = "github:msalmanrafadhlih/nixos-development-templates/main?dir=rust";
+    crane-tauri.url = "github:JPHutchins/crane-tauri";
+    devenv.url = "github:cachix/devenv";
 
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
-    };
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-compat.follows = "flake-compat";
-      };
-    };
-
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        git-hooks.follows = "git-hooks";
-      };
     };
 
     android-nixpkgs = {
@@ -39,7 +20,12 @@
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }@inputs:
+    {
+      nixpkgs,
+      flake-utils,
+      crane-tauri,
+      ...
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -50,8 +36,83 @@
             allowUnfree = true;
           };
         };
+        inherit (pkgs) lib;
+        crane = inputs.rust.inputs.crane;
+        craneLib = crane.mkLib pkgs;
+
+        frontend =
+          let
+            nodeModules = pkgs.stdenvNoCC.mkDerivation {
+              pname = "my-app-frontend-deps";
+              version = "0.1.0";
+              src = lib.fileset.toSource {
+                root = ./.;
+                fileset = lib.fileset.unions [
+                  ./package.json
+                  ./bun.lock
+                ];
+              };
+              nativeBuildInputs = [ pkgs.bun ];
+              dontConfigure = true;
+              buildPhase = ''
+                export HOME=$TMPDIR
+                bun install --frozen-lockfile --no-progress
+              '';
+              installPhase = "mkdir -p $out: cp -r node_modules $out/";
+              outputHasMode = "recursive";
+              outputHashAlgo = "sha256";
+              outputHash = lib.fakeHash; # change to the real one after `nix build`
+            };
+          in
+          pkgs.stdenvNoCC.mkDerivation {
+            pname = "my-app-frontend";
+            version = "0.1.0";
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./packages.json
+                ./bun.lock
+                ./tsconfig.json
+                ./vite.config.ts
+                ./index.html
+                ./public
+                ./src
+              ];
+            };
+            nativeBuildInputs = [ pkgs.bun ];
+            dontConfigure = true;
+            buildPhase = ''
+              export HOME=$TMPDIR
+              cp -r ${nodeModules}/node_modules .
+              chmod -R u+w node_modules
+              bun run build
+            '';
+            installPhase = "cp -r dist $out";
+          };
+
+        tauri = crane-tauri.lib.buildTauriApp { inherit pkgs craneLib; } {
+          pname = "my-app";
+          version = "0.1.0";
+          src = ./.;
+          # cargoRoot = ./.; # if `src-tauri/Cargo.toml` depends on sibling creates by relative path
+          inherit frontend;
+        };
       in
       {
+        packages.default = tauri.app;
+        checks = {
+          inherit (tauri) app;
+
+          clippy = craneLib.cargoClippy (
+            tauri.commonArgs
+            // {
+              cargoArtifacts = tauri.cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- -D warnings";
+              TAURI_CONFIG = tauri.tauriConfig;
+            }
+          );
+        };
+
         devShells = {
           default = inputs.devenv.lib.mkShell {
             inherit inputs pkgs;
