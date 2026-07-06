5e423662a0aafb44f23b98237062144d9017a8db
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-06 20:59:56 +0700

    Update via Auto Commit - 2026-07-06 20:59
    Update file: rustling/flake.nix
    
    Changes:
    -    nixpkgs.follows = "root-flake/nixpkgs";
    -    flake-utils.follows = "root-flake/flake-utils";
    -    rust = "github:msalmanrafadhlih/nixos-development-templates/main?dir=rust";
    -            (import ./devenv.nix { templateInputs = inputs; })
    +

--- rustling/flake.nix
+++ rustling/flake.nix
@@ -2,9 +2,10 @@
   description = "Rust Learning Environment";
 
   inputs = {
-    nixpkgs.follows = "root-flake/nixpkgs";
-    flake-utils.follows = "root-flake/flake-utils";
-    rust = "github:msalmanrafadhlih/nixos-development-templates/main?dir=rust";
+    rust.url = "github:msalmanrafadhlih/nixos-development-templates/main?dir=rust";
+    nixpkgs.follows = "rust/nixpkgs";
+    flake-utils.follows = "rust/flake-utils";
+    devenv.follows = "rust/devenv";
   };
 
   outputs =
@@ -25,11 +26,20 @@
           inherit inputs pkgs;
           modules = [
             inputs.rust.devenvModules.default
-            (import ./devenv.nix { templateInputs = inputs; })
             ({ pkgs, ... }: {
               packages = [
                 pkgs.rustlings
               ];
+              # Contoh Scripts untuk mempermudah belajar
+              scripts.hint.exec = "rustlings hint $1";
+              scripts.watch.exec = "rustlings watch";
+
+              # Menampilkan pesan saat masuk shell
+              enterShell = ''
+                echo "--- 📚 RUST LEARNING MODE ---"
+                echo "Toolchain: $(rustc --version)"
+                echo "Tersedia command: 'watch' untuk mulai, 'hint <no>' untuk bantuan."
+              '';
             })
           ];
         };
